24770e31cfd7c7b0df58ecb4e5c6d6b0c8a7ae33
Chester Sim <chestersimdy@gmail.com>
2021-03-21 17:31:56 +0800

    Add node env variable

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const bot = require('./telegram');
 const hostname = '127.0.0.1';
 const port = 1111;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const server = http.createServer();
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
