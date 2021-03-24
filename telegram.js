a169354e5a3f676bf67ed74fb04ba0d3846ee24b
ChesterSim <chestersimdy@gmail.com>
2021-03-24 17:31:18 +0800

    Fix edit last queue number bug

--- telegram.js
+++ telegram.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const fsPromises = require('fs').promises;
+
 const { scrape } = require('./btoScraper');
+const db = require('./db.json');
 
 const token = process.env.TELEGRAM_TOKEN;
 
@@ -8,19 +11,27 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, async (msg) => {
   bot.sendMessage(msg.chat.id, "Please wait a couple of minutes while we scrape the data for you!");
-  const highUnitsLeftPromise = scrape();
+  const highUnitsLeft = await scrape();
 
-  bot.sendMessage(msg.chat.id, "In the meantime, let me know how what is the last queue number.")
+  const lastQueueNumber = db.lastQueueNumber;
+  const potentialQueueBetween = Math.trunc((874 - lastQueueNumber) * 0.82);
+  const potentialHighFloorsToSelect = highUnitsLeft - potentialQueueBetween;
 
-  bot.on('message', async (msg) => {
-    const lastQueueNumber = msg.text;
-    const potentialQueueBetween = Math.trunc((874 - lastQueueNumber) * 0.82);
-    const highUnitsLeft = await highUnitsLeftPromise;
-    const potentialHighFloorsToSelect = highUnitsLeft - potentialQueueBetween;
-
-    bot.sendMessage(msg.chat.id, 'Number of high units (6 and above) remaining: ' + highUnitsLeft);
-    bot.sendMessage(msg.chat.id, 'Potential number of high floors left to select from: ' + potentialHighFloorsToSelect);
-  })
+  bot.sendMessage(msg.chat.id, 'Number of high units (6 and above) remaining: ' + highUnitsLeft);
+  bot.sendMessage(msg.chat.id, 'Potential number of high floors left to select from: ' + potentialHighFloorsToSelect);
 });
+
+bot.onText(/\/q/, async (msg) => {
+  const lastQueueNumber = parseInt(msg.text.split(' ')[1]);
+
+  if (lastQueueNumber === NaN) {
+    bot.sendMessage(msg.chat.id, "Sorry, please give the last queue number as such - '/q 758'")
+  } else {
+    db.lastQueueNumber = lastQueueNumber;
+    await fsPromises.writeFile('./db.json', JSON.stringify(db));
+
+    bot.sendMessage(msg.chat.id, `The last queue number, ${lastQueueNumber} is updated!`);
+  }
+})
 
 module.exports = bot;
